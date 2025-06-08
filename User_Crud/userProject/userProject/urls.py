333b0b432e9e3fa640580d4976829c855cd791f7
rijumia <riju.mia2612@gmail.com>
2025-06-08 12:35:36 +0600

    Add user management views and update URL patterns for add and list pages

--- User_Crud/userProject/userProject/urls.py
+++ User_Crud/userProject/userProject/urls.py
@@ -5,4 +5,7 @@ from userProject.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homePage, name='home'),
+    path('add/', addPage, name='addPage'),
+    path('list/', listPage, name='listPage'),
+    
 ]
--- User_Crud/userProject/userProject/views.py
+++ User_Crud/userProject/userProject/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def homePage(request):
     return render(request, 'home.html')
+
+def addPage(request):
+    return render(request, 'addUser.html')
+
+def listPage(request):
+    return render(request, 'userList.html')
+
+def editPage(request):
+    return render(request, 'userUpdate.html')
+
+def viewPage(request):
+    return render(request, 'userDetails.html')
