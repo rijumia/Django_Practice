5bbe2bcbc54f25e87b07f82ead3fc1969450a322
rijumia <riju.mia2612@gmail.com>
2025-06-12 20:28:00 +0600

    Update userModel field lengths and change string representation to return Last_Name

--- User_Crud/userProject/userApp/models.py
+++ User_Crud/userProject/userApp/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class userModel(models.Model):
-    First_Name = models.CharField(max_length=100)
-    Last_Name = models.CharField(max_length=100)
+    First_Name = models.CharField(max_length=50)
+    Last_Name = models.CharField(max_length=50)
     Full_Name = models.CharField(max_length=200)
     User_Name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return self.Full_Name
+        return self.Last_Name
     
 
