40439b530ac4092f7f3901941acd27ed6dc8c901
rijumia <riju.mia2612@gmail.com>
2025-07-01 20:53:58 +0600

    Implement user registration form handling in registerPage view

--- Institute/instituteProject/instituteApp/views.py
+++ Institute/instituteProject/instituteApp/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from instituteApp.models import CustomUserModel, TeacherModel, StudentModel
 
 def registerPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        fullname = request.POST.get('fullname')
+        email = request.POST.get('email')
+        contact = request.POST.get('contact')
+        address = request.POST.get('address')
+        usertype = request.POST.get('usertype')
+        designation = request.POST.get('designation')
+        department = request.POST.get('department')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+
+        if password == confirm_password:
+            user = Cus
+
     return render(request, 'register.html')
