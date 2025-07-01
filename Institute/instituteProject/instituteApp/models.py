e90903544bb85a80b5483787674a00e662acb3d1
rijumia <riju.mia2612@gmail.com>
2025-07-01 20:47:45 +0600

    Fix user_type field definition in CustomUserModel to include choices parameter

--- Institute/instituteProject/instituteApp/models.py
+++ Institute/instituteProject/instituteApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUserModel(AbstractUser):
-    user_type = models.CharField([
+    user_type = models.CharField(choices=[
         ('teacher', 'Teacher'),
         ('student', 'Student'),
     ], max_length=10, null=True)
