d424b83e380464c2c645443a3b6c2f2311d7bd4b
rijumia <riju.mia2612@gmail.com>
2025-07-01 20:42:14 +0600

    Register CustomUserModel with custom admin configuration

--- Institute/instituteProject/instituteApp/admin.py
+++ Institute/instituteProject/instituteApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from instituteApp.models import CustomUserModel, TeacherModel, StudentModel
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'user_type')
+
+admin.site.register(CustomUserModel, CustomUserAdmin)
+admin.site.register(TeacherModel)
+admin.site.register(StudentModel)
