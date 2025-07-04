14ae1930dd9dd2a9b5abf3ab3098e11c032ad50b
rijumia <riju.mia2612@gmail.com>
2025-07-04 16:56:59 +0600

    Refactor user and job models to include user type and job type fields

--- Job_Finder/JobProject/JobApp/models.py
+++ Job_Finder/JobProject/JobApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUserModel(AbstractUser):
+    user_type = models.CharField(choices=[
+        ('recruiter', 'recruiter'),
+        ('seeker', 'Seeker')
+    ], max_length=10, null=True)
+    phone_no = models.CharField(max_length=15, null=True)
+    Bio = models.TextField(null=True)
+
+class JobModel(models.Model):
+    JobTitle = models.CharField(max_length=50, null=True)
+    JobDescription = models.TextField(null=True)
+    CompanyLogo = models.ImageField(upload_to='Media/ComapnyLogo',null=True)
+    Job_type = models.CharField(choices=[
+        ('full_time', 'Full Time'),
+        ('part_time','Part Time'),
+    ], max_length=10,null=True)
+    CreateBy = models.ForeignKey(CustomUserModel,on_delete=models.CASCADE,null=True)
+
