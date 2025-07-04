920657a32d49fe2de405e49e780f0c5c9ab09acb
rijumia <riju.mia2612@gmail.com>
2025-07-04 17:50:18 +0600

    Add job feed and home page templates; implement views and update URL routing

--- Job_Finder/JobProject/JobApp/views.py
+++ Job_Finder/JobProject/JobApp/views.py
@@ -12,3 +12,13 @@ def changePasswordPage(request):
 
 def logoutPage(request):
     return redirect('loginPage')
+
+
+def homePgae(request):
+    return render(request, 'home.html')
+
+def jobFeedPgae(request):
+    return render(request, 'jobFeed.html')
+
+def addJobPgae(request):
+    return render(request, 'addJob.html')
--- Job_Finder/JobProject/JobProject/urls.py
+++ Job_Finder/JobProject/JobProject/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('register/', signupPage, name='signupPage'),
     path('changePassword/', changePasswordPage, name='changePasswordPage'),
     path('logoutPage/', logoutPage, name='logoutPage'),
+
+    path('homePgae/', homePgae, name='homePgae'),
+    path('jobFeedPgae/', jobFeedPgae, name='jobFeedPgae'),
+    path('addJobPgae/', addJobPgae, name='addJobPgae'),
 ]
